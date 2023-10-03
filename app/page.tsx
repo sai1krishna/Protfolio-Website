e45c8d54761057a1961b0155cee25f90f6f0a8f4
sai1krishna <saikrishnas1411@gmail.com>
2023-10-03 03:23:42 -0400

    second commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import About from "@/components/about";
 import Contact from "@/components/contact";
-import Education from "@/components/Education";
+import Education from "@/components/education";
 import Experience from "@/components/experience";
 import Intro from "@/components/intro";
 import Projects from "@/components/projects";
